846c41bbaa5d40459094778f1568afce8ef666be
simon <simon@debian.debian_simon>
2018-10-14 16:32:33 +0200

    acces data

--- tensorflow/begin_with_tensorflow/intro_to_pandas.py
+++ tensorflow/begin_with_tensorflow/intro_to_pandas.py
@@ -18,3 +18,21 @@ city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
 population = pd.Series([852469, 1015785, 485199])
 
 pd.DataFrame({ 'City name': city_names, 'Population': population })
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe.describe()
+california_housing_dataframe.head()
+california_housing_dataframe.hist('housing_median_age')
+
+"""
+    Accéder aux données
+    L'accès aux données d'un DataFrame s'effectue au moyen d'opérations de liste ou de dictionnaire Python courantes
+"""
+cities = pd.DataFrame({ 'City name': city_names, 'Population': population })
+print(type(cities['City name']))
+cities['City name']
+
+print(type(cities['City name'][1]))
+cities['City name'][1]
+
+print(type(cities[0:2]))
+cities[0:2]
